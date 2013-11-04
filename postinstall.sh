049d0b919b5e3f33a9a96d9e32297a14d97d2ef9
Charlie Hoover <cphoover@gmail.com>
2013-11-04 13:14:23 -0500

    changing package structure added path to bin modified postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,20 @@
-echo -e  -e '#!'"$(which node)\n$(cat ./minify-js-closure)" > ./minify-js-closure
-echo -e  -e '#!'"$(which node)\n$(cat ./minify-js)"         > ./minify-js
-echo -e  -e '#!'"$(which node)\n$(cat ./minify-css)"        > ./minify-css
+function remove_first_line(){
+    FILE=$1
+    mv $FILE $FILE.tmp
+    tail -n +2 $FILE.tmp > $1
+    rm $FILE.tmp 
+}
+
+function add_shebang(){
+    APP=$1
+    FILE=$2
+    echo -e '#!'"$(which $APP)\n$(cat ./$FILE)" > $FILE
+}
+
+remove_first_line ./bin/minify-js-closure
+remove_first_line ./bin/minify-js
+remove_first_line ./bin/minify-css
+
+add_shebang node ./bin/minify-js-closure
+add_shebang node ./bin/minify-js
+add_shebang node ./bin/minify-css
